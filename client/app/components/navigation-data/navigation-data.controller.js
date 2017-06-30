4ec78c1132d8cfeeb8cd385600afe64fc904a354
Rui Vilela <Carolina@MacBook-Pro.local>
2017-06-30 21:33:14 +0200

    filter pages

--- client/app/components/navigation-data/navigation-data.controller.js
+++ client/app/components/navigation-data/navigation-data.controller.js
@@ -8,13 +8,54 @@ export default class NavigationDataCtrl {
         for(let i = 0 ; i < json.length ; i++){
             if(json[i].customerID == $stateParams.customerID){
               $scope.data.push(json[i]);
-            }
-            $scope.$apply()
-        }
-      })
+            };
+        };
+        $scope.$apply();
+      });
+
+      fetch('http://localhost:8000/api/Customers/' + $stateParams.customerID)
+        .then((result) => result.json())
+        .then((json) => {
+          $scope.name = json.firstName;
+          $scope.$apply();
+        });
 
     $scope.backToOverview = () => {
-      $location.url('/')
+      $location.url('/');
+    }
+
+    $scope.sortalphabetically = () => {
+      switch($scope.pageorder){
+        case "ascending":
+          $scope.data = $scope.data.sort(descending)
+          $scope.pageorder = "descending"
+          break;
+        default:
+          $scope.data = $scope.data.sort(ascending);
+          $scope.pageorder = "ascending"
+          break;
+      }
     }
   }
+}
+
+// functions
+const ascending = (a,b) =>  {
+  if (a.pages < b.pages){
+    return -1;
+  }
+  if (a.pages > b.pages){
+    return 1;
+  }
+  return 0;
+}
+
+const descending = (a,b) =>  {
+  if (a.pages < b.pages){
+    return 1;
+  }
+  if (a.pages > b.pages){
+    return -1;
+  }
+  return 0;
 }
